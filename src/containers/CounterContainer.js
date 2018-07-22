e176e3267d7064bd5e5aafbae414297cde6747dc
JoGyuHyeion <kuhyun1993@naver.com>
2018-07-22 17:58:12 +0900

    single counter

---
+++ src/containers/CounterContainer.js
@@ -0,0 +1,42 @@
+import Counter from '../components/Counter';
+import * as actions from '../actions';
+import { connect } from 'react-redux';
+import { getRandomColor } from '../utils';
+
+// store 안의 state 값을 props 로 연결해줍니다.
+const mapStateToProps = (state) => ({
+    color: state.color,
+    number: state.number
+});
+
+
+/* 
+    액션 생성자를 사용하여 액션을 생성하고,
+    해당 액션을 dispatch 하는 함수를 만들은 후, 이를 props 로 연결해줍니다.
+*/
+
+const mapDispatchToProps = (dispatch) => ({
+    onIncrement: () => dispatch(actions.increment()),
+    onDecrement: () => dispatch(actions.decrement()),
+    onSetColor: () => {
+        const color = getRandomColor();
+        dispatch(actions.setColor(color));
+    }
+});
+
+// Counter 컴포넌트의 Container 컴포넌트
+// Counter 컴포넌트를 어플리케이션의 데이터 레이어와 묶는 역할을 합니다.
+
+//connect 함수는 파라미터로 컴포넌트에 연결시킬 상태, 액션함수 전달하면 
+//리덕스 스토어에 연결시키는 또다른 함수를 반환한다.
+//리턴됨 함수 안에 프리젠테이셔널 컴포넌트를 파라미터를 전달해주면 리덕스 스토어에 연결된 컴포넌트가 새로 만들어진다.
+// connect 
+//1. state,action_function 등 redux-store 연결
+//2. 연결후 리턴된 새로운 객체에 프리젠테이션 컴포넌트 연결
+const CounterContainer = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Counter);
+
+
+export default CounterContainer;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+
+// Redux 관련 불러오기
+import { createStore } from 'redux'
+import reducers from './reducers';
+import { Provider } from 'react-redux';
+
+// 스토어 생성
+const store = createStore(reducers);
+
+ReactDOM.render(
+  //Provider 는 react-redux 의 내장되어 있는 컴포넌트 store를 쉽게 연동할수 있도록 도와준다
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
+registerServiceWorker();
