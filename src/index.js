167f6698b21794103ee1054490841b1f85126f83
JoGyuHyeion <kuhyun1993@naver.com>
2018-07-24 01:30:05 +0900

    multi-counter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './containers/App';
 import './index.css';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
 
 // Redux 관련 불러오기
 import { createStore } from 'redux'
@@ -13,9 +12,8 @@ import { Provider } from 'react-redux';
 const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
-  //Provider 는 react-redux 의 내장되어 있는 컴포넌트 store를 쉽게 연동할수 있도록 도와준다
-  <Provider store={store}>
-    <App />
-  </Provider>
-  , document.getElementById('root'));
-registerServiceWorker();
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
